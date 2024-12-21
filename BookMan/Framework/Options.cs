5db24bf18a71d3985a523382dfeb184c373f3b1f
Pham Nghia <nghiapham.050503@gmail.com>
2024-12-21 16:15:47 +0700

    Thêm class Options để kiểm tra hợp lệ của các options tuyền vào request

---
+++ BookMan/Framework/Options.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace BookMan.ConsoleApp.Framework
+{
+    /// <summary>
+    /// Class static lưu toàn bộ options của các lệnh
+    /// </summary>
+    public static class Options
+    {
+        /// <summary>
+        /// Lưu danh sách option cho từng lệnh, key là lệnh, value là danh sách option
+        /// </summary>
+        public static Dictionary<string, string[]> ListOptions = new Dictionary<string, string[]>();
+
+        /// <summary>
+        /// Thêm danh sách options
+        /// </summary>
+        /// <param name="key">Lệnh</param>
+        /// <param name="value">Danh sách option</param>
+        public static void AddListOptions(string key, string[] value)
+        {
+            ListOptions.Add(key, value);
+        }
+
+        /// <summary>
+        /// Lấy danh sách options của lệnh
+        /// </summary>
+        /// <param name="key">Lệnh</param>
+        /// <returns></returns>
+        public static List<string> GetOptions(string key)
+        {
+            if (ListOptions.TryGetValue(key, out var value))
+                return new List<string>(value);
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Kiểm tra option có thuộc lệnh hay không?
+        /// </summary>
+        /// <param name="key">Lệnh</param>
+        /// <param name="option">option</param>
+        /// <returns></returns>
+        public static bool CheckOptions(string key, string option)
+        {
+            var list = GetOptions(key);
+            if (list != null)
+                return list.Contains(option);
+            else
+                return false;
+        }
+    }
+}
